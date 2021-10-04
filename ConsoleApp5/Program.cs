d9a057fd4d5184580fff4d3cf631a288e8fa5df0
Nijatasgar99 <nijataas@code.edu.az>
2021-10-05 00:10:53 +0500

    Добавьте файлы проекта.

---
+++ ConsoleApp5/Program.cs
@@ -0,0 +1,77 @@
+﻿using System;
+
+namespace ConsoleApp5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            #region
+            //int tek = 0, cut = 0;
+            //for (int i = 1; i <= 26; i++)
+            //{
+            //    if (i%2==0)
+            //    {
+            //        cut += i;
+            //    }
+            //}
+            //Console.WriteLine(cut);
+            #endregion
+            #region
+            //Sinifdə işlədiyimiz taskı bir daha təkrar yazmaq.
+            //int input = 48;
+            //int result = input;
+            //if (input % 11 == 0)
+            //{
+
+            //    Console.WriteLine($"{input} 11e bolunur");
+
+            //}
+            //else
+            //{
+            //    while (result % 11 != 0)
+            //    {
+            //        result--;
+            //    }
+
+            //}
+            //Console.WriteLine(result);
+            #endregion
+            #region
+            //Sinifdə işlədiyimiz taskı bir daha təkrar yazmaq.
+
+            //int input = 30;
+            //    int result = input;
+            //if (input % 8 == 0) 
+            //{
+            //    Console.WriteLine($"{input} 8e bolunur");
+            //}
+            //else
+            //{
+            //    int qaliq = input % 8;
+            //    if (qaliq> 2)
+            //    {
+            //        while (result % 8 != 0)
+            //        {
+            //            result++;
+            //        }
+            //    }
+            //    else
+            //    {
+            //        while (result % 8 != 0)
+            //        {
+            //            result--;
+            //        }
+            //    }
+            //    Console.WriteLine(result);
+            //}
+            #endregion
+            
+
+
+
+        }
+
+    }
+ }
+
